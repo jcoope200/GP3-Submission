ba218432589042ee68c4060588c522bacc5c224a
Jamie Cooper <jcoope200@caledonian.ac.uk>
2014-12-16 16:32:33 +0000

    missing .cpp files

---
+++ GP3 Coursework/GP3 Coursework/GameConstants.cpp
@@ -0,0 +1,16 @@
+/*
+==================================================================================
+GameConstants.cpp
+==================================================================================
+*/
+
+#include "GameConstants.h"
+
+int drawMode = 0;
+float rotationAngle = 0;
+float translationZ = 0;
+bool fire = false;
+bool topDown = true;
+bool bgmPlaying = true;
+int health = 5;
+bool dead = false;
